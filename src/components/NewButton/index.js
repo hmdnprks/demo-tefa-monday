d9f6e111737eb2dbd337675c6fce6aa5034af1ed
Hamdan Prakoso <hamdan.prakoso@mail.ugm.ac.id>
2023-05-15 11:43:42 +0700

    add random color functionality to new button

--- src/components/NewButton/index.js
+++ src/components/NewButton/index.js
@@ -1,9 +1,23 @@
+import { useState } from 'react';
 import styles from './style.module.css';
+import { randomColor } from '../../utils/randomColor';
 
 export default function NewButton() {
+
+  const [color, setColor] = useState('blue');
+
+  const setRandomColor = () => {
+    const modColor = randomColor();
+    setColor(modColor);
+  }
+
   return (
-    <button className={styles.pink}>
-      Hai, apa Kabar!
-    </button>
+    <div>
+      <button className={styles.pink} style={{
+        color: color
+      }} onClick={setRandomColor}>
+        Hai, klik aku untuk mengganti warna
+      </button>
+    </div>
   )
 }
